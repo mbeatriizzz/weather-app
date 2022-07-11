0f0f78538b5fb29e6cb476b630ac7981187be1aa
Mbeatriizzz <beatriz.maia@unifesspa.edu.br>
2022-07-10 23:20:59 -0300

    Preparando ambiente

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+const api = {
+  key: "1763adce2ed10c6da45c8991ab3367ad",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          
+        </div>
+      </main>
+     
     </div>
   );
 }
